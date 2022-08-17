8531d6f8f8554f6d902ba7951daa3b015c1ed598
Omar Badawi <itsomar278@gmail.com>
2022-08-17 16:20:07 +0300

    visual studio project added

---
+++ Kata/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+
