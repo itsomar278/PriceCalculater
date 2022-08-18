ec87b5411dd8e05f7d7b99c11f07a6ed90d288f6
Omar Badawi <itsomar278@gmail.com>
2022-08-18 04:42:13 +0300

    product class added , validations added

---
+++ Kata/Product.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class Product
+    {
+        public Product(String Name , long UPC , decimal price)
+        {
+            if(!Name.IsProductName() ||!UPC.IsUPC() ||!price.IsPrice() )
+            {
+                throw new ArgumentException("check the product data entered "); 
+            }
+            _Name = Name;
+            _UPC = UPC;
+            _Price = price;
+
+        }
+     
+        public string _Name;
+        public long _UPC;
+        public decimal _Price; 
+        public decimal Price
+        {
+            get 
+            { 
+                return _Price;
+            }
+            private set 
+            {
+                if (value.IsPrice())
+                {
+                    _Price = value;
+                }
+                throw new ArgumentException("invalid price"); 
+            }
+        }
+        public long UPC
+        {
+            get
+            {
+                return _UPC;
+            }
+            private set
+            {
+                if (value.IsUPC())
+                {
+                    _UPC = value;
+                }
+                throw new ArgumentException("UPC is invalid");
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+           private set
+            {
+               if (value.IsProductName())
+                {
+                    _Name = value;  
+                }
+
+            }
+
+        }
+
+
+     
+
+
+    }
+}
---
+++ Kata/Validations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal static class Validations
+    {
+        public static bool IsUPC (this long upc )
+        {
+           
+            if(upc.ToString().Length != 5 )
+            {
+                return false;
+            }
+            if (upc < 0 )
+            { 
+                return false;
+            }
+            return true; 
+        }
+        public static bool IsPrice (this decimal price )
+        {
+            if(price == 0 || price < 0 )
+            {
+                return false; 
+            }
+            return true; 
+
+        }
+        public static bool IsProductName(this string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                return false; 
+            }
+            return true; 
+        }
+
+
+    }
+}
