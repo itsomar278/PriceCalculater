e1a3f8fd5b08389fbb957fd31ef1efff186bbeb1
Omar Badawi <itsomar278@gmail.com>
2022-08-21 11:19:50 +0300

    REPORT : extension method DiscountReport added in new static class Report

---
+++ Kata/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public static class Report
+    {
+        public static string DiscountReport(this Product product)
+        {
+          return($"$ {product.price.DiscountAmount} amount which was deduced");
+        }
+    }
+}
